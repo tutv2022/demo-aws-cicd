7002824f8b8f73b34b276ddb618a02d113204049
tutv2022 <tucute89@gmail.com>
2023-02-08 18:12:10 +0700

    Test New helloWorld

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,12 +15,12 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World 1";
+		return "Hello World TuTV";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World 1");
+		return new HelloWorldBean("Hello World TuTV");
 	}
 	
 	///hello-world/path-variable/in28minutes
